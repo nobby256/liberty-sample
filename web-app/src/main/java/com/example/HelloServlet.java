e9f1851e469c47ae969ffdf4a3bfa052ca4c5d1a
nobby256 <helloworld_1024@yahoo.co.jp>
2026-03-08 23:48:55 +0900

    更新

--- web-app/src/main/java/com/example/HelloServlet.java
+++ web-app/src/main/java/com/example/HelloServlet.java
@@ -18,7 +18,7 @@ public class HelloServlet extends HttpServlet {
 
         resp.setContentType("text/html; charset=UTF-8");
         PrintWriter out = resp.getWriter();
-        out.println("<html><body>Hello World</body></html>");
+        out.println("<html><body>Hello World222</body></html>");
     }
 
     @Override
